0512365294f7d4fd6018b1ea8c6d2c37b06ee7e9
veljan.r <95553450+RamoVeljan@users.noreply.github.com>
2021-12-27 20:40:59 +0100

    Add files via upload

---
+++ vezba 1/script.js
@@ -0,0 +1,40 @@
+
+var auta = [
+    {
+        img:`https://www.askramar.com/Vozilo?id=23731`,
+        naziv:`VW Passat`,
+        cijena:`37.500 KM`,
+        godiste:`2017`,
+        kilometraza:`137.300`,
+    },
+    {
+        img:`https://www.askramar.com/Vozilo?id=23727`,
+        naziv:`Peugeot 2008`,
+        cijena:`23.990 KM`,
+        godiste:`2017`,
+        kilometraza:`148.994`,
+    },
+    {
+        img:`https://www.askramar.com/Vozilo?id=23690`,
+        naziv:`Peugeot 3008`,
+        cijena:`39.990 KM`,
+        godiste:`2019`,
+        kilometraza:`149.502`,
+    },
+    {
+        img:`https://www.askramar.com/Vozilo?id=23673`,
+        naziv:`Audi Q2`,
+        cijena:`42.990 KM`,
+        godiste:`2018`,
+        kilometraza:`139.813`,
+    }
+]
+
+function ce(el){
+    return document.createElement(`el`);
+}
+
+function displayCard(data){
+    var div = ce("div")
+
+}
